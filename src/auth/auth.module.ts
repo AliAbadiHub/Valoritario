a57d4a3f93c06abd404bedf64d124cecec37b800
AliAbadiHub <ali_abadi1980@hotmail.com>
2023-02-27 13:22:19 -0400

    connected to redis with host port and pw

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport/dist';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
@@ -6,9 +6,20 @@ import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [UsersModule, PassportModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    UsersModule,
+    PassportModule,
+    TypeOrmModule.forFeature([User]),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+      password: 'password',
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
 })
